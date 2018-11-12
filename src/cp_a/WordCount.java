19b79e11a4cca620edcb9141612e7cc32194487b
tug72675 <tug72675@DESKTOP-1Q0C2B9.cis.temple.edu.>
2018-11-11 23:38:50 -0500

    serial parsing done

--- src/cp_a/WordCount.java
+++ src/cp_a/WordCount.java
@@ -1,5 +1,7 @@
 package cp_a;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -14,42 +16,69 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class WordCount {
 
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
-
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            
-            Scanner scn=new Scanner(value.toString());
-            while(scn.hasNext())
-            {
-                String Line=scn.nextLine();
-                int index1=value.find("\"id\"");
-                
+
+            StringTokenizer strtok = new StringTokenizer(value.toString(), "\n");
+            if (strtok.hasMoreTokens()) {
+                //strtok.nextToken();
             }
             
-            HashSet<String> ID=new HashSet<String>();
-            
-            
-            
+            context.write(new Text("value size"), new Text(value.getLength()+""));
+
+            while (strtok.hasMoreTokens()) {
+                String Line = strtok.nextToken();
+
+                JSONParser parser = new JSONParser();
+                try {
+
+                    JSONObject obj = (JSONObject) parser.parse(Line);
+
+                    JSONObject payload = (JSONObject) obj.get("payload");
+                    //System.out.println(payload);
+
+                    JSONArray pages = (JSONArray) payload.get("page");
+
+                    for (int j = 0; j < pages.size(); j++) {
+                        JSONObject comment = (JSONObject) pages.get(j);
+                        // System.out.println(comment);
+                        long id = (long) comment.get("id");
+                        // System.out.println(id);
+                        Text ID = new Text(Long.toString(id));
+                        Text COMMENT = new Text(comment.toString());
+
+                        context.write(ID, COMMENT);
+
+                    }
+
+                } catch (ParseException pe) {
+
+                }
+
+            }
+
         }
     }
 
-    public static class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    public static class IntSumReducer extends Reducer<Text, Text, Text, Text> {
 
-        private IntWritable result = new IntWritable();
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+            for (Text val : values) {
+                context.write(new Text(), val);
+             
+                break;
             }
-            result.set(sum);
-            context.write(key, result);
+    
+
         }
     }
 
@@ -63,7 +92,9 @@ public class WordCount {
         job.setReducerClass(WordCount.IntSumReducer.class);
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
